54a789559266d0861ffd32f3a10cc910c249d9a1
ramadan15 <ramazanaltinisik16@gmail.com>
2024-01-10 20:06:51 +0300

    10. ödev eklendi
    10. ödevi ekledim

---
+++ 10. ödev .sql
@@ -0,0 +1,12 @@
+-- 1. Aşama 
+--select city, country from city
+--left join country on city.country_id= country.country_id
+-- 2. Aşama 
+--select first_name, last_name, payment_id from customer
+--right join payment on customer.customer_id= payment.customer_id
+--order by payment_id
+-- 3. aşama
+--select first_name, last_name, rental_id from customer 
+--full join rental on customer.customer_id= rental.customer_id
+-- order by rental_id
+
